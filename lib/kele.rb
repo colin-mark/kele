371c7c731e02ddbe7e10e99d08b7b98d4ae10df3
colin_mark <colin.alcorn@gmail.com>
2018-06-10 16:26:50 -0500

    User Story 3 Mentor Availability: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,14 +4,19 @@ class Kele
   include HTTParty
 
   def initialize(email, password)
-    @base_url = "https://www.bloc.io/api/v1"
+    @base_url = "https://www.bloc.io/api/v1/"
     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
     raise "invalid email/pass, bud" if response.code != 200
     @auth_token = response["auth_token"]
   end
 
   def get_me
-    url = @base_url + "/users/me"
+    url = @base_url + "users/me"
     JSON.parse(self.class.get(url, headers: { "authorization" => @auth_token }).body)
   end
+
+  def get_mentor_availability(mentor_id)
+		url = @base_url + "mentors/#{mentor_id}/student_availability"
+		JSON.parse(self.class.get(url, headers: {"authorization" => @auth_token}).body)
+	end
 end
