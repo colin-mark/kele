20da7ab76661891c2d459306f7dde3857a5034ff
colin_mark <colin.alcorn@gmail.com>
2018-06-16 16:04:32 -0500

    User Story 6 Remaining Checkpoints: completed

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -8,4 +8,9 @@ class Kele
     url = @base_url + "checkpoints/#{checkpoint_id}"
     JSON.parse(self.class.get(url, headers: {"authorization" => @auth_token}).body)
   end
+
+  def get_remaining_checkpoints(chain_id)
+    url = @base_url + "enrollment_chains/#{chain_id}/checkpoints_remaining_in_section"
+    JSON.parse(self.class.get(url, headers: {"authorization" => @auth_token}).body)
+  end
 end
